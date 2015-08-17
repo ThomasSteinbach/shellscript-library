457feeefe14a7ffefcdd57fa715b01f74b662173
Thomas Steinbach <thomas.steinbach@aikq.de>
2015-08-17 18:38:36 +0200

    common copy script

--- iteraCopy/iteratec2thomass.sh
+++ iteraCopy/iteratec2thomass.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-cd /home/thomass/git/thomass_ansible_roles
-rm -rf thomass.common thomass.docker thomass.java thomass.mysql thomass.nodejs thomass.playbook2dockerimage
-cd /etc/ansible/roles
-cp -a thomass.common thomass.docker thomass.java thomass.mysql thomass.nodejs thomass.playbook2dockerimage /home/thomass/git/thomass_ansible_roles
-cd /home/thomass/git/thomass_ansible_roles
-git status
+copyRoles.sh "/home/thomass/git/lab.iteratec.de/ansible_roles" "/home/thomass/git/lab.xarif.de/ansible_roles"
--- iteraCopy/thomass2iteratec.sh
+++ iteraCopy/thomass2iteratec.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-cd /etc/ansible/roles
-rm -rf thomass.common thomass.docker thomass.java thomass.mysql thomass.nodejs thomass.playbook2dockerimage
-cd /home/thomass/git/thomass_ansible_roles
-cp -a thomass.common thomass.docker thomass.java thomass.mysql thomass.nodejs thomass.playbook2dockerimage /etc/ansible/roles
-cd /etc/ansible/roles
-git status
+./copyRoles.sh "/home/thomass/git/lab.xarif.de/ansible_roles" "/home/thomass/git/lab.iteratec.de/ansible_roles"
