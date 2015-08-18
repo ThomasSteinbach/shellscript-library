0cb10382444af502cb35bf3fd6ee07a863eaeb22
Thomas Steinbach <thomas.steinbach@aikq.de>
2015-08-18 21:13:17 +0200

    added new role

--- iteraCopy/copyRoles.sh
+++ iteraCopy/copyRoles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$2"
-rm -rf thomass.common thomass.docker thomass.java thomass.mysql thomass.nodejs thomass.playbook2dockerimage
+rm -rf thomass.common thomass.docker thomass.java thomass.mysql thomass.mysql_connector thomass.nodejs thomass.playbook2dockerimage
 cd "$1"
-cp -a thomass.common thomass.docker thomass.java thomass.mysql thomass.nodejs thomass.playbook2dockerimage "$2"
+cp -a thomass.common thomass.docker thomass.java thomass.mysql thomass.mysql_connector thomass.nodejs thomass.playbook2dockerimage "$2"
 cd "$2"
 git status
--- iteraCopy/iteratec2thomass.sh
+++ iteraCopy/iteratec2thomass.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-copyRoles.sh "/home/thomass/git/lab.iteratec.de/ansible_roles" "/home/thomass/git/lab.xarif.de/ansible_roles"
+./copyRoles.sh "/home/thomass/git/lab.iteratec.de/ansible_roles" "/home/thomass/git/lab.xarif.de/ansible_roles"
