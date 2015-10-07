dca431d3ce05319bebb65c26717384e6e9bb6bc5
Thomas Steinbach <thomas.steinbach@aikq.de>
2015-10-07 14:43:00 +0200

    renaming of role

--- iteraCopy/copyRoles.sh
+++ iteraCopy/copyRoles.sh
@@ -2,6 +2,6 @@
 cd "$2"
 rm -rf thomass.common thomass.docker thomass.java thomass.mysql thomass.mysql_connector thomass.nodejs thomass.playbook2dockerimage thomass.gradle thomass.install_docker_daemon
 cd "$1"
-cp -a thomass.common thomass.docker thomass.java thomass.mysql thomass.mysql_connector thomass.nodejs thomass.playbook2dockerimage thomass.gradle thomass.install_docker_daemon "$2"
+cp -a thomass.common thomass.docker thomass.java thomass.mysql thomass.mysql_connector thomass.nodejs thomass.playbook2dockerimage thomass.gradle thomass.install_docker_service "$2"
 cd "$2"
 git status
