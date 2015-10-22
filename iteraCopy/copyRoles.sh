9515e15436d9a92b507debd35e58124514978a97
Thomas Steinbach <thomas.steinbach@aikq.de>
2015-10-22 21:57:17 +0200

    iteracopy not longer used

--- iteraCopy/copyRoles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd "$2"
-rm -rf thomass.common thomass.docker thomass.java thomass.mysql thomass.mysql_connector thomass.nodejs thomass.playbook2dockerimage thomass.gradle thomass.install_docker_daemon thomass.ruby
-cd "$1"
-cp -a thomass.common thomass.docker thomass.java thomass.mysql thomass.mysql_connector thomass.nodejs thomass.playbook2dockerimage thomass.gradle thomass.install_docker_service thomass.ruby "$2"
-cd "$2"
-git status
--- iteraCopy/thomass2iteratec.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./copyRoles.sh "/home/thomass/git/lab.xarif.de/ansible_roles" "/home/thomass/git/lab.iteratec.de/ansible_roles"
