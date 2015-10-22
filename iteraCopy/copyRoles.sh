f2d5c3b8ffff74e2f24a285009cf14f10e6e56c4
Thomas Steinbach <thomas.steinbach@aikq.de>
2015-10-22 16:30:49 +0200

    add ruby

--- iteraCopy/copyRoles.sh
+++ iteraCopy/copyRoles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$2"
-rm -rf thomass.common thomass.docker thomass.java thomass.mysql thomass.mysql_connector thomass.nodejs thomass.playbook2dockerimage thomass.gradle thomass.install_docker_daemon
+rm -rf thomass.common thomass.docker thomass.java thomass.mysql thomass.mysql_connector thomass.nodejs thomass.playbook2dockerimage thomass.gradle thomass.install_docker_daemon thomass.ruby
 cd "$1"
-cp -a thomass.common thomass.docker thomass.java thomass.mysql thomass.mysql_connector thomass.nodejs thomass.playbook2dockerimage thomass.gradle thomass.install_docker_service "$2"
+cp -a thomass.common thomass.docker thomass.java thomass.mysql thomass.mysql_connector thomass.nodejs thomass.playbook2dockerimage thomass.gradle thomass.install_docker_service thomass.ruby "$2"
 cd "$2"
 git status
