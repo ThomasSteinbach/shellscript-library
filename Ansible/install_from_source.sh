6303a1f2e1730758cf72ab1449857afcfe7689ca
Thomas Steinbach <thomas.steinbach@aikq.de>
2018-01-20 13:17:47 +0100

    removed Ansible install script

--- Ansible/install_from_source.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ~
-git clone git://github.com/ansible/ansible.git --recursive
-sudo apt-get install python-pip
-sudo pip install --upgrade paramiko PyYAML Jinja2 httplib2 six docker-py
-echo "source ~/ansible/hacking/env-setup -q" >> ~/.bashrc
-source ~/ansible/hacking/env-setup -q
-echo "Ansible installed:"
-ansible --version
